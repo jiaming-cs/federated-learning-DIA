ff14ae5e4bc549cf8f9bede0bc09919b2ff389ff
Jiaming Li <jiamingli9674@gmail.com>
2021-06-04 10:15:29 -0700

    fix bug

---
+++ diagnosis_w100_lstm_training.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 22:15:25 2020
+
+@author: aaronli
+"""
+
+import loader
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import sklearn.metrics
+from sklearn.metrics import roc_curve, auc, confusion_matrix
+from sklearn.preprocessing import label_binarize
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.tree import DecisionTreeClassifier
+import pickle
+import numpy as np
+import datetime
+
+# Running time calculate: start time
+start_time = datetime.datetime.now()
+
+# Hyper Parameters
+EPOCH = 300
+BATCH_SIZE = 128
+TIME_STEP = 100  # length of LSTM time sequence, or window size
+INPUT_SIZE = 6 # num of feature for deep learning
+LR = 0.001   # learning rate
+KFOLD = 10
+isGPU = torch.cuda.is_available()
+
+# detection deep learning dataset path
+#data_path_DL = '../new_clean_data/w120_final_dataset/fault_detection/deep_learning/'
+#all_data_DL = loader.waveformDetectionDL(data_path_DL)
+
+# diagnosis deep learning datasset path
+data_path_DL = '../new_clean_data/w100_final_dataset/fault_diagnosis/deep_learning/'
+all_data_DL = loader.waveformDiagnosisDL(data_path_DL)
+
+
+#%%----------------------create the LSTM Net ------------------------------------
+class LSTM(nn.Module):
+    def __init__(self):
+        super(LSTM, self).__init__()
+        
+        self.lstm = nn.LSTM(
+                input_size=INPUT_SIZE,
+                hidden_size=32,
+                num_layers=5,
+                batch_first=True,
+                )
+        #fully connected
+        #self.out = nn.Linear(32, 9)
+        self.out = nn.Linear(32, 9)
+    
+    def forward(self, x):
+        lstm_out, (h_n, h_c) = self.lstm(x, None)
+        out = self.out(lstm_out[:, -1, :])
+        return out
+
+
+data_output = {'lstm':{'F1':[], 'precision':[], 'recall':[], 'accuracy':[], 'auc':[], 'fpr':[], 'tpr':[], 'test_loss':[], 'train_loss':[], 'confusion_mat':[]}}
+
+for num_of_training in range(KFOLD):
+    print('------------------fold {}------------------------'.format(num_of_training + 1))
+    lstm = LSTM()
+    
+    if isGPU:
+        lstm = nn.DataParallel(lstm, device_ids=[0,1,2,3])
+        lstm.cuda()
+
+    lstm_optimizer = torch.optim.Adam(lstm.parameters(), lr=LR)
+    loss_func = nn.CrossEntropyLoss()
+    
+    # print the structure of the network
+    print(lstm)
+    
+    # data partition: 15% testing, 85% training
+    training_data, test_data = torch.utils.data.random_split(all_data_DL, [int(all_data_DL.len * 0.85), all_data_DL.len - int(all_data_DL.len * 0.85)])
+    training_Loader = DataLoader(dataset=training_data, batch_size=BATCH_SIZE, shuffle=True)
+    test_Loader = DataLoader(dataset=test_data, batch_size=BATCH_SIZE)
+    
+    # training and testing
+    lstm_test_loss_draw = []
+    
+    lstm_loss_draw = []
+    
+    for epoch in range(EPOCH):
+        print('-----------------------epoch {}----------------------'.format(epoch + 1))
+        
+        # training-----------------------------------------
+        lstm_train_loss = 0.
+        
+        lstm_train_acc = 0.
+        
+        lstm.train()
+        
+        for step, (batch_x, batch_y) in enumerate(training_Loader):
+            batch_x = batch_x.view(-1, TIME_STEP, INPUT_SIZE)
+            
+            if isGPU:
+                batch_x = batch_x.cuda()
+                batch_y = batch_y.cuda()
+
+            output_lstm = lstm(batch_x)
+          
+            loss_lstm = loss_func(output_lstm, batch_y)
+            
+            lstm_train_loss += loss_lstm.item()
+            
+            
+            
+            if isGPU:
+                lstm_pred = torch.max(output_lstm, 1)[1].cuda()
+            else:
+                lstm_pred = torch.max(output_lstm, 1)[1]
+            
+            lstm_train_correct = (lstm_pred == batch_y).sum()
+            
+            lstm_train_acc += lstm_train_correct.item()
+            
+            lstm_optimizer.zero_grad()
+            
+            loss_lstm.backward()
+            
+            lstm_optimizer.step()
+            
+        print('LSTM:\n Train Loss: {:.6f}, Accuracy: {:.6f}\n'.format(lstm_train_loss / 
+              (len(training_data)), lstm_train_acc / (len(training_data))))
+        
+        lstm_loss_draw.append(lstm_train_loss/(len(training_data)))
+        
+        
+        # evaluation--------------------------------------------------
+        lstm.eval()
+        
+        lstm_eval_loss = 0.
+        
+        lstm_eval_acc = 0.
+        
+        lstm_final_prediction = np.array([])
+        lstm_final_test = np.array([])
+        lstm_f1_score = []
+        lstm_recall = []
+        lstm_precision = []
+        lstm_accuracy = []
+      
+        for step, (batch_x, batch_y) in enumerate(test_Loader):
+            batch_x = batch_x.view(-1, TIME_STEP, INPUT_SIZE)
+            
+            if isGPU:
+                batch_x = batch_x.cuda()
+                batch_y = batch_y.cuda()
+    
+            output_lstm = lstm(batch_x)
+            
+            loss_lstm = loss_func(output_lstm, batch_y)
+            
+            lstm_eval_loss += loss_lstm.item()
+            
+            lstm_pred = torch.max(output_lstm, 1)[1]
+            
+            lstm_eval_correct = (lstm_pred == batch_y).sum()
+            
+            
+            if isGPU:
+                lstm_pred = torch.max(output_lstm, 1)[1].cuda()
+            else:
+                lstm_pred = torch.max(output_lstm, 1)[1]
+            
+            lstm_eval_acc += lstm_eval_correct.item()
+                      
+            # F1 metrics
+            lstm_final_prediction = np.concatenate((lstm_final_prediction, lstm_pred.cpu().numpy()), axis=0)
+            lstm_final_test = np.concatenate((lstm_final_test, batch_y), axis=0)
+        
+        current_f1 = sklearn.metrics.f1_score(lstm_final_test, lstm_final_prediction, average='weighted').item()
+        current_precision = sklearn.metrics.precision_score(lstm_final_test, lstm_final_prediction, average='weighted', zero_division=1).item()
+        lstm_f1_score.append(current_f1)
+        lstm_precision.append(current_precision)
+        lstm_recall.append((current_f1*current_precision) / (2*current_precision - current_f1))
+        lstm_accuracy.append(lstm_eval_acc / (len(test_data)))
+        
+        
+
+        
+        print('LSTM:\n Test Loss: {:.6f}, Accuracy: {:.6f}'.format(lstm_eval_loss / (len(test_data)), lstm_eval_acc / (len(test_data))))
+        
+        lstm_test_loss_draw.append(lstm_eval_loss/(len(test_data)))
+        
+        print('LSTM:\n F1: {}, recall: {}, precision: {}'.format(lstm_f1_score[-1], lstm_recall[-1], lstm_precision[-1]))
+    
+    # confusing matrix
+    current_cm = confusion_matrix(list(lstm_final_test), list(lstm_final_prediction))
+    print(current_cm)
+    
+    # ROC curve and AUC
+    #lstm_test_y = label_binarize(lstm_final_test, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+    #lstm_pred_y = label_binarize(lstm_final_prediction, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+    lstm_test_y = label_binarize(lstm_final_test, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+    lstm_pred_y = label_binarize(lstm_final_prediction, classes=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+    
+    lstm_fpr, lstm_tpr, _ = roc_curve(lstm_test_y.ravel(), lstm_pred_y.ravel())
+    lstm_roc_auc = auc(lstm_fpr, lstm_tpr)
+    
+    
+    data_output['lstm']['F1'].append(lstm_f1_score[-1])
+    data_output['lstm']['precision'].append(lstm_precision[-1])
+    data_output['lstm']['recall'].append(lstm_recall[-1])
+    data_output['lstm']['accuracy'].append(lstm_accuracy[-1])
+    data_output['lstm']['auc'].append(lstm_roc_auc.item())
+    data_output['lstm']['fpr'].append(list(lstm_fpr))
+    data_output['lstm']['tpr'].append(list(lstm_tpr))
+    data_output['lstm']['test_loss'].append(lstm_test_loss_draw)
+    data_output['lstm']['train_loss'].append(lstm_loss_draw)  
+    
+    data_output['lstm']['confusion_mat'].append(current_cm)
+
+#%% ---------------------dataoutput------------------------------------------------------------------
+
+for i in range(KFOLD):
+    print('--------Fold {}----------------'.format(i))
+
+    print('LSTM: F1:{}, prec:{}, rec:{}, acc:{}, auc:{}'.format(data_output['lstm']['F1'][i],
+          data_output['lstm']['precision'][i], data_output['lstm']['recall'][i],
+          data_output['lstm']['accuracy'][i], data_output['lstm']['auc'][i]))
+    
+    # save figures for ann, cnn, lstm
+    
+    plt.figure()
+    
+    plt.plot(data_output['lstm']['test_loss'][i], label='LSTM testing')
+    plt.plot(data_output['lstm']['train_loss'][i], label='LSTM training')
+
+    
+    plt.legend()
+    
+    plt.xlabel('Epoch')
+    plt.xticks(np.arange(0, EPOCH+1, EPOCH/10))
+    plt.ylabel('Loss')
+    plt.title('Loss Function')
+    plt.savefig('../fig/w100_detection/'+'Loss_Kfold_LSTM_w100_diagnosis_'+str(i+1)+'.png',dpi=500)
+    
+    # save figures of ROC curve
+    plt.figure()
+    plt.plot(data_output['lstm']['fpr'][i], data_output['lstm']['tpr'][i], label='LSTM (AUC = {0:0.2f})'.format(data_output['lstm']['auc'][i]))
+
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.legend()
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC curve')
+    plt.savefig('../fig/w100_detection/'+'ROC__Kfold_LSTM_w100_diagnosis_'+str(i+1)+'.png',dpi=500)
+    
+    
+pickle_out = open('LSTM_w100_10fold_diagnosis.pickle', 'wb')
+pickle.dump(data_output, pickle_out)
+pickle_out.close()
+
+# end time
+end_time = datetime.datetime.now()
+print('Running time: {}'.format(end_time - start_time))
+
---
+++ w100_ml_detection.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  7 22:15:25 2020
+
+@author: aaronli
+"""
+
+import loader
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import sklearn.metrics
+from sklearn.metrics import roc_curve, auc, confusion_matrix
+from sklearn.preprocessing import label_binarize
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC, SVR
+from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import preprocessing
+import pickle
+import numpy as np
+import datetime
+
+
+# Running time calculate: start time
+start_time = datetime.datetime.now()
+
+# Hyper Parameters
+EPOCH = 20
+BATCH_SIZE = 32
+TIME_STEP = 100  # length of LSTM time sequence, or window size
+INPUT_SIZE = 6 # num of feature for deep learning
+LR = 0.01   # learning rate
+KFOLD = 1
+isGPU = torch.cuda.is_available()
+
+# detection deep learning dataset path
+#data_path_FT = '../new_clean_data/w200_final_dataset/fault_diagnosis/feature/'
+
+#all_data_FT = loader.waveformDiagnosisFeature(data_path_FT)
+
+# detection deep learning dataset path
+data_path_FT = '../new_clean_data/w100_final_dataset/fault_detection/feature/'
+all_data_FT = loader.waveformDetectionFeature(data_path_FT)
+
+
+data_output = {'knn':{'F1':[], 'precision':[], 'recall':[], 'accuracy':[], 'auc':[], 'fpr':[], 'tpr':[], 'confusion_mat':[]}, 
+               'dtree':{'F1':[], 'precision':[], 'recall':[], 'accuracy':[], 'auc':[], 'fpr':[], 'tpr':[], 'confusion_mat':[]}, 
+               'svm':{'F1':[], 'precision':[], 'recall':[], 'accuracy':[], 'auc':[], 'fpr':[], 'tpr':[], 'confusion_mat':[]}}
+
+for num_of_training in range(KFOLD):
+    
+    # machine learning methods
+    ml_X = all_data_FT.x_data.numpy()
+    ml_X = ml_X.reshape(ml_X.shape[0], -1)
+    ml_Y = all_data_FT.y_data.numpy()
+    
+    # scale X for normalization
+#     min_max_scaler = preprocessing.MinMaxScaler()
+#     ml_X = min_max_scaler.fit_transform(ml_X)
+#     print(ml_Y)
+#     print(ml_X[0], ml_X[1])
+    
+    ml_X_train, ml_X_test, ml_Y_train, ml_Y_test = train_test_split(ml_X, ml_Y, test_size=0.15)
+    
+#     # train dtree
+    dtree_model = DecisionTreeClassifier(max_depth = 12).fit(ml_X_train, ml_Y_train)
+    dtree_pred = dtree_model.predict(ml_X_test)
+    
+    # train SVM 
+    #svm_model = SVC(kernel = 'rbf', C = 1, max_iter = 100).fit(ml_X_train, ml_Y_train)
+    svm_model = SVC(kernel = 'rbf', max_iter = 10).fit(ml_X_train, ml_Y_train)
+    #svm_model = SVR(C=1.0, epsilon=0.2, max_iter=1000).fit(ml_X_train, ml_Y_train)
+    svm_pred = svm_model.predict(ml_X_test)
+    
+    #svm_pred_training = svm_model.predict(ml_X_train)
+#     # train KNN 
+    knn_model = KNeighborsClassifier(n_neighbors = 17).fit(ml_X_train, ml_Y_train)
+    knn_pred = knn_model.predict(ml_X_test)
+    
+#     # metrics for machine learning methods
+    dtree_f1 = sklearn.metrics.f1_score(ml_Y_test, dtree_pred, average='binary')
+    dtree_precision = sklearn.metrics.precision_score(ml_Y_test, dtree_pred, average='binary')
+    #dtree_recall = sklearn.metrics.recall_score(ml_Y_test, dtree_pred, average='weighted')
+    #dtree_recall = ((dtree_f1*dtree_precision) / (2*dtree_precision - dtree_f1))
+    dtree_recall = sklearn.metrics.recall_score(ml_Y_test, dtree_pred, average='binary')
+    dtree_acc = sklearn.metrics.accuracy_score(ml_Y_test, dtree_pred)
+    
+    svm_f1 = sklearn.metrics.f1_score(ml_Y_test, svm_pred, average='binary')
+    svm_precision = sklearn.metrics.precision_score(ml_Y_test, svm_pred, average='binary')
+    svm_recall = sklearn.metrics.recall_score(ml_Y_test, svm_pred, average='binary')
+    #svm_recall = ((svm_f1*svm_precision) / (2*svm_precision - svm_f1))
+    svm_acc = sklearn.metrics.accuracy_score(ml_Y_test, svm_pred)
+    
+    knn_f1 = sklearn.metrics.f1_score(ml_Y_test, knn_pred, average='binary')
+    knn_precision = sklearn.metrics.precision_score(ml_Y_test, knn_pred, average='binary')
+    knn_recall = sklearn.metrics.recall_score(ml_Y_test, knn_pred, average='binary')
+    #knn_recall = ((knn_f1*knn_precision) / (2*knn_precision - knn_f1))
+    knn_acc = sklearn.metrics.accuracy_score(ml_Y_test, knn_pred)
+
+    # confusion matrix
+    dtree_current_cm = confusion_matrix(ml_Y_test, dtree_pred)
+    svm_current_cm = confusion_matrix(ml_Y_test, svm_pred)
+    knn_current_cm = confusion_matrix(ml_Y_test, knn_pred)
+    
+    print("dtree_matrix:\n{}\n".format(dtree_current_cm))
+    print("svm_matrix:\n{}\n".format(svm_current_cm))
+    print("knn_matrix:\n{}\n".format(knn_current_cm))
+    
+#     # for ROC curve
+    svm_test_y = label_binarize(ml_Y_test, classes=[0, 1])
+    svm_pred_y = label_binarize(svm_pred, classes=[0, 1])
+    svm_fpr, svm_tpr, _ = roc_curve(svm_test_y.ravel(), svm_pred_y.ravel())
+    svm_roc_auc = sklearn.metrics.auc(svm_fpr, svm_tpr)
+    #svm_roc_auc = sklearn.metrics.auc(svm_fpr, svm_tpr)
+    
+    knn_test_y = label_binarize(ml_Y_test, classes=[0, 1])
+    knn_pred_y = label_binarize(knn_pred, classes=[0, 1])
+    knn_fpr, knn_tpr, _ = roc_curve(knn_test_y.ravel(), knn_pred_y.ravel())
+    knn_roc_auc = sklearn.metrics.auc(knn_fpr, knn_tpr)
+    
+    dtree_test_y = label_binarize(ml_Y_test, classes=[0, 1])
+    dtree_pred_y = label_binarize(dtree_pred, classes=[0, 1])
+    dtree_fpr, dtree_tpr, _ = roc_curve(dtree_test_y.ravel(), dtree_pred_y.ravel())
+    dtree_roc_auc = sklearn.metrics.auc(dtree_fpr, dtree_tpr)
+    
+#     # machine learning method output
+    data_output['knn']['F1'].append(knn_f1)
+    data_output['knn']['precision'].append(knn_precision)
+    data_output['knn']['recall'].append(knn_recall)
+    data_output['knn']['accuracy'].append(knn_acc)
+    data_output['knn']['auc'].append(knn_roc_auc.item())
+    data_output['knn']['fpr'].append(list(knn_fpr))
+    data_output['knn']['tpr'].append(list(knn_tpr))
+    data_output['knn']['confusion_mat'].append(knn_current_cm)
+    
+    data_output['dtree']['F1'].append(dtree_f1)
+    data_output['dtree']['precision'].append(dtree_precision)
+    data_output['dtree']['recall'].append(dtree_recall)
+    data_output['dtree']['accuracy'].append(dtree_acc)
+    data_output['dtree']['auc'].append(dtree_roc_auc.item())
+    data_output['dtree']['fpr'].append(list(dtree_fpr))
+    data_output['dtree']['tpr'].append(list(dtree_tpr))
+    data_output['dtree']['confusion_mat'].append(dtree_current_cm)
+    
+    data_output['svm']['F1'].append(svm_f1)
+    data_output['svm']['precision'].append(svm_precision)
+    data_output['svm']['recall'].append(svm_recall)
+    data_output['svm']['accuracy'].append(svm_acc)
+    data_output['svm']['auc'].append(svm_roc_auc.item())
+    data_output['svm']['fpr'].append(list(svm_fpr))
+    data_output['svm']['tpr'].append(list(svm_tpr))
+    data_output['svm']['confusion_mat'].append(svm_current_cm)
+    
+for i in range(KFOLD):
+    print('--------Fold {}----------------'.format(i))
+
+
+    print('KNN: F1:{}, prec:{}, rec:{}, acc:{}'.format(data_output['knn']['F1'][i],
+           data_output['knn']['precision'][i], data_output['knn']['recall'][i],
+           data_output['knn']['accuracy'][i]))
+    print('SVM: F1:{}, prec:{}, rec:{}, acc:{}'.format(data_output['svm']['F1'][i],
+           data_output['svm']['precision'][i], data_output['svm']['recall'][i],
+           data_output['svm']['accuracy'][i]))
+    print('Dtree: F1:{}, prec:{}, rec:{}, acc:{}'.format(data_output['dtree']['F1'][i],
+           data_output['dtree']['precision'][i], data_output['dtree']['recall'][i],
+           data_output['dtree']['accuracy'][i]))
+
+pickle_out = open('ML_w100_10fold_detection.pickle', 'wb')
+pickle.dump(data_output, pickle_out)
+pickle_out.close()            
+            
+# end time
+end_time = datetime.datetime.now()
+print('Running time: {}'.format(end_time - start_time))
+    
