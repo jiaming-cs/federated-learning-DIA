eb9949f3d9372455785bb28c2285f9586c74a43d
Jiaming <jiamingli9674@gmail.com>
2021-06-22 12:07:57 -0700

    fix data label

--- flower/split_data.py
+++ flower/split_data.py
@@ -14,6 +14,8 @@ def split_data(input_data, client_num, iid=True, abnormal_fraction = 0.75, works
         with open(os.path.join(workspace_dir, splited_data_folder, f'test_data.pkl'), 'wb') as f:
             pickle.dump(dict(x_data=x_test, y_data=y_test), f)
 
+            print(f"Test Set: P:{len(x_test[y_test==1])}, N:{len(x_test[y_test==0])}")
+
         x_data_normal, x_data_abnormal = x_train[y_train==0], x_train[y_train==1]
         num_normal_client_0, num_abnormal_client_0 = int(len(x_data_normal) * (1-abnormal_fraction)), int(len(x_data_abnormal) * abnormal_fraction)
         num_normal_other, num_abnormal_other = (len(x_data_normal) - num_normal_client_0) // (client_num - 1), (len(x_data_abnormal) - num_abnormal_client_0) // (client_num - 1)
@@ -22,18 +24,17 @@ def split_data(input_data, client_num, iid=True, abnormal_fraction = 0.75, works
             with open(os.path.join(workspace_dir, splited_data_folder, f'data_{i}.pkl'), 'wb') as f:
                 if i == 0:
                     x_data = np.concatenate([x_data_abnormal[:num_abnormal_client_0], x_data_normal[:num_normal_client_0]])
-                    y_data = np.concatenate([np.zeros((num_abnormal_client_0,)), np.ones((num_normal_client_0,))])
+                    y_data = np.concatenate([np.ones((num_abnormal_client_0,)), np.zeros((num_normal_client_0,))])
                     pickle.dump(dict(x_data=x_data, y_data=y_data), f)
-
+                    print(f"Train Set {i}: P:{len(x_data[y_data==1])}, N:{len(x_data[y_data==0])}")
                     x_data_abnormal, x_data_normal = x_data_abnormal[num_abnormal_client_0:], x_data_normal[num_normal_client_0:]
                 
                 else:
                     
                     x_data = np.concatenate([x_data_abnormal[(i-1)*num_abnormal_other: i*num_abnormal_other], x_data_normal[(i-1)*num_normal_other: i*num_normal_other]])
-                    y_data = np.concatenate([np.zeros((num_abnormal_other,)), np.ones((num_normal_other,))])
+                    y_data = np.concatenate([np.ones((num_abnormal_other,)), np.zeros((num_normal_other,))])
                     pickle.dump(dict(x_data=x_data, y_data=y_data), f)
-
-            print(f"split data {i}")
+                    print(f"Train Set {i}: P:{len(x_data[y_data==1])}, N:{len(x_data[y_data==0])}")
     else:
         x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.1, random_state=42)
         with open(os.path.join(workspace_dir, splited_data_folder, f'test_data.pkl'), 'wb') as f:
